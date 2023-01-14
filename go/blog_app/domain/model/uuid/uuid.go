cc12817a55f5b708ef2d770c9946ca9460e949b8
SDTakeuchi <kuranku191952996@gmail.com>
2023-01-14 13:56:28 +0900

    update uuid

--- go/blog_app/domain/model/uuid/uuid.go
+++ go/blog_app/domain/model/uuid/uuid.go
@@ -9,19 +9,12 @@ type UUID struct {
 	uuid.UUID
 }
 
-func NewV4(ctx context.Context) (UUID, error) {
-	uid, err := uuid.NewV4()
-	if err != nil {
-		log.Errorf(ctx, "failed to generate UUID: %v", err)
-		return UUID{}, err
-	}
-	return UUID{uid}, nil
+func New(ctx context.Context) UUID {
+	id := uuid.New()
+	return UUID{id}
 }
 
 func FromString(uuidString string) (UUID, error) {
-	u, err := uuid.FromString(uuidString)
-	if err != nil {
-		return UUID{u}, err
-	}
-	return UUID{u}, nil
+	u, err := uuid.Parse(uuidString)
+	return UUID{u}, err
 }
