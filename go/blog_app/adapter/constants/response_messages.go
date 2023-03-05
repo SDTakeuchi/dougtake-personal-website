5c4fbe3212d8c535827248d6f634ce2c3b4423a5
SDTakeuchi <kuranku191952996@gmail.com>
2023-03-05 14:04:39 +0900

    add error res

--- go/blog_app/adapter/constants/response_messages.go
+++ go/blog_app/adapter/constants/response_messages.go
@@ -3,6 +3,13 @@ package constants
 type ResponseMessage string
 
 var (
+	AuthenticationFailed ResponseMessage = "authentication failed"
+	InvalidToken         ResponseMessage = "token is invalid"
+	ExpiredToken         ResponseMessage = "token is already expired"
+
+	PasswordTooShort  ResponseMessage = "password must be at least 8 characters"
+	IncorrectPassword ResponseMessage = "incorrect password"
+
 	DefaultErrorMessage ResponseMessage = "unexpected error has occurred: contact to the owner"
 	RecordNotFound      ResponseMessage = "record not found"
 	FailedToBindQuery   ResponseMessage = "failed to bind query: wrong format"
