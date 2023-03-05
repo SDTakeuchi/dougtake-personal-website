aef85b83d144d3368580cc84e84f0682218891b9
SDTakeuchi <kuranku191952996@gmail.com>
2023-03-05 14:05:58 +0900

    switch env to yaml

--- go/blog_app/adapter/config/config.go
+++ go/blog_app/adapter/config/config.go
@@ -2,49 +2,68 @@ package config
 
 import (
 	"log"
+	"os"
 	"time"
 
-	"github.com/kelseyhightower/envconfig"
+	"github.com/go-yaml/yaml"
 )
 
 type (
 	DB struct {
-		Database    string `required:"true" envconfig:"DB_NAME"`
-		UserName    string `required:"true" envconfig:"DB_USER_NAME"`
-		Password    string `required:"true" envconfig:"DB_PASSWORD"`
-		Port        string `required:"true" envconfig:"DB_PORT" default:"5432"`
-		HostName    string `required:"true" envconfig:"DB_HOST_NAME"`
-		SSLMode     string `required:"true" envconfig:"DB_SSL_MODE"`
-		ConnTimeout int    `required:"true" envconfig:"DB_CONN_TIMEOUT" default:"9"`
+		Database    string `required:"true" yaml:"name"`
+		UserName    string `required:"true" yaml:"user_name"`
+		Password    string `required:"true" yaml:"password"`
+		Port        string `required:"true" yaml:"port" default:"5432"`
+		HostName    string `required:"true" yaml:"host_name"`
+		SSLMode     string `required:"true" yaml:"ssl_mode"`
+		ConnTimeout int    `required:"true" yaml:"conn_timeout" default:"9"`
 		// 0 means no termination
-		MaxOpen int `required:"true" envconfig:"DB_MAX_OPEN" default:"0"`
-		MaxIdle int `required:"true" envconfig:"DB_MAX_IDLE" default:"2"`
+		MaxOpen int `required:"true" yaml:"max_open" default:"0"`
+		MaxIdle int `required:"true" yaml:"max_idle" default:"2"`
 		// 0 means no termination as long as the process is running
 		// set values that can be read by time.ParseDuration()
-		MaxLifeTime time.Duration `required:"true" envconfig:"DB_MAX_LIFE_TIME" default:"0"`
+		MaxLifeTime time.Duration `required:"true" yaml:"max_life_time" default:"0"`
 	}
 
 	Frontend struct {
-		HostName string `required:"true" envconfig:"FRONTEND_HOST_NAME"`
-		Port     string `required:"true" envconfig:"FRONTEND_PORT" default:"8000"`
+		HostName string `required:"true" yaml:"host_name"`
+		Port     string `required:"true" yaml:"port" default:"8000"`
 	}
 
 	API struct {
-		Port string `required:"true" envconfig:"GO_PORT" default:"8080"`
+		Port string `required:"true" yaml:"port" default:"8080"`
+	}
+
+	Auth struct {
+		SecretKey            string        `required:"true" yaml:"secret_key"`
+		AccessTokenDuration  time.Duration `required:"true" yaml:"access_token_duration"`
+		RefreshTokenDuration time.Duration `required:"true" yaml:"refresh_token_duration"`
+		MinSecretKeySize     int           `required:"true" yaml:"min_secret_key_size"`
 	}
 
 	Config struct {
-		Debug    bool `required:"true" envconfig:"DEBUG" default:"false"`
-		DB       DB
-		Frontend Frontend
-		API      API
+		Debug    bool     `required:"true" yaml:"debug" default:"false"`
+		DB       DB       `yaml:"db"`
+		Frontend Frontend `yaml:"frontend"`
+		API      API      `yaml:"api"`
+		Auth     Auth     `yaml:"auth"`
 	}
 )
 
 var globalConfig Config
 
 func Load() {
-	if err := envconfig.Process("", &globalConfig); err != nil {
+	f, err := os.Open("config.yaml")
+	if err != nil {
+		log.Fatalf(
+			"load config fialed: os.Open err: %v",
+			err.Error(),
+		)
+	}
+	defer f.Close()
+
+	err = yaml.NewDecoder(f).Decode(&globalConfig)
+	if err != nil {
 		log.Fatalf(
 			"err: %v, globalConfig: %+v",
 			err.Error(),
