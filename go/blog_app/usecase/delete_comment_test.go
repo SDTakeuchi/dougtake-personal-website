22e2a6f4b4cd2b65c236e73b9cb19b3b693a9911
ShuheiTakeuchi <shuhei.takeuchi@kyash.co>
2023-04-30 12:13:01 +0900

    delete comment test impled

--- go/blog_app/usecase/delete_comment_test.go
+++ go/blog_app/usecase/delete_comment_test.go
@@ -1,8 +1,11 @@
 package usecase
 
 import (
-	"blog_app/domain/repository"
+	mockrepo "blog_app/domain/repository/mock"
+	testutil "blog_app/util/test"
 	"context"
+	"github.com/golang/mock/gomock"
+	"gorm.io/gorm"
 	"reflect"
 	"testing"
 )
@@ -12,24 +15,91 @@ func Test_deleteCommentImpl_Execute(t *testing.T) {
 		ctx   context.Context
 		input DeleteCommentInput
 	}
+
+	randomTags := testutil.GenRandomTags(2)
+
+	tagIDs := func() []uint64 {
+		var ids []uint64
+		for _, t := range randomTags {
+			ids = append(ids, t.ID())
+		}
+		return ids
+	}()
+	randomPosts := testutil.GenRandomPosts(2, tagIDs)
+	postIDs := func() []uint64 {
+		var ids []uint64
+		for _, p := range randomPosts {
+			ids = append(ids, p.ID())
+		}
+		return ids
+	}()
+	randomComments := testutil.GenRandomComments(4, postIDs)
+
+	fakeID := uint64(100000000000)
+
 	tests := []struct {
-		name    string
-		u       *deleteCommentImpl
-		args    args
-		want    *DeleteCommentOutput
-		wantErr bool
+		name              string
+		args              args
+		buildStubsComment func(mockComment *mockrepo.MockComment)
+		want              *DeleteCommentOutput
+		wantErr           bool
 	}{
-		// TODO: Add test cases.
+		{
+			"success",
+			args{
+				context.Background(),
+				DeleteCommentInput{
+					randomComments[0].ID(),
+				},
+			},
+			func(mockComment *mockrepo.MockComment) {
+				mockComment.EXPECT().
+					Delete(gomock.Any(), randomComments[0].ID()).
+					Times(1).
+					Return(
+						nil,
+					)
+			},
+			&DeleteCommentOutput{},
+			false,
+		},
+		{
+			"fail/id-not-found",
+			args{
+				context.Background(),
+				DeleteCommentInput{
+					fakeID,
+				},
+			},
+			func(mockComment *mockrepo.MockComment) {
+				mockComment.EXPECT().
+					Delete(gomock.Any(), fakeID).
+					Times(1).
+					Return(
+						gorm.ErrRecordNotFound,
+					)
+			},
+			nil,
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.u.Execute(tt.args.ctx, tt.args.input)
+			ctrl := gomock.NewController(t)
+
+			mockComment := mockrepo.NewMockComment(ctrl)
+			tt.buildStubsComment(mockComment)
+
+			deleteComment := NewDeleteComment(
+				mockComment,
+			)
+			got, err := deleteComment.Execute(tt.args.ctx, tt.args.input)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("deleteCommentImpl.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("updateCommentImpl.Execute() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("deleteCommentImpl.Execute() = %v, want %v", got, tt.want)
+				t.Errorf("updateCommentImpl.Execute() = %v, want %v", got, tt.want)
 			}
 		})
 	}
